ecd0b52fc3da242112d100a795dcb061b61d422f
shyksubhan <isubhan375@gmail.com>
2026-07-02 09:34:38 +0500

    SEO: optimized meta tags, schema markup, sitemap, robots.txt, dynamic product meta

--- js/products-render.js
+++ js/products-render.js
@@ -48,7 +48,7 @@ function velorraProductCardHTML(p) {
 
   return `
     <div class="product-card reveal" data-category="${p.category}" data-id="${p.id}">
-      <a href="product?id=${encodeURIComponent(p.id)}">
+      <a href="product?id=${encodeURIComponent(p.id)}&name=${p.name ? p.name.toLowerCase().replace(/[^a-z0-9]+/g,'-').replace(/^-|-$/g,'') : ''}">
         <div class="product-img-wrap">
           ${badge}
           ${mediaHTML}
